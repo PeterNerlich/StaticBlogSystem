a66d928a73583cc4bceffbdf7fe9914f3443ce5a
Peter Nerlich <peter@peter-ubuntu.fritz.box>
2016-06-01 02:33:59 +0200

    shortened last part according to present

--- article/2016-05-15-test01/info.php
+++ article/2016-05-15-test01/info.php
@@ -1,5 +1,7 @@
 <?php
 
+# self explanatory
+
 $THIS->title = array(
 	'EN' => "Blog Test 01",
 	'DE' => "Blog Test 01"
@@ -15,11 +17,13 @@ $THIS->keywords	=	array(
 	'EN' => array('test','php','html','css','hack'),
 	'DE' => array('test','php','html','css','hack')
 );
+# this will be shown if no `preview.php` is present
 $THIS->preview = array(
 	'EN' => "<div class='bubble'><p>I am just building my own <b>Blog</b>, but it is different from other systems out there. I really like to write my websites completely by myself, with <b>100% of hand-written code</b>. In terms of the blog posts I would also like to have as much of this freedom as possible, while on the other hand I would like to have a search functionality. Also, I do not have another MYSQL database available at the moment.</p></div>",
 	'DE' => "<div class='bubble'><p>Ich baue gerade meinen eigenen <b>Blog</b>, der anders wird als bekannte bestehende Systeme. Ich bevorzuge es, meine Internetseiten selbst zu schreiben, m <b>100% handgeschriebenem Code</b>. Für die Blog Posts würde ich auch gerne so frei wie möglich sein, während ich auch gerne eine Suchfunktion hätte. Außerdem ist für mich gerade keine MYSQL-Datenbank verfügbar.</p></div>"
 );
 
+# returning true to test wether everything loaded correctly
 return TRUE;
 
 ?>
