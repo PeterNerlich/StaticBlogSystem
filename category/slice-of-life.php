a90b669a225632af40ca554d2046b1e28d82d0ad
Peter Nerlich <peter.nerlich@googlemail.com>
2016-06-07 00:04:16 +0200

    renamed category Personal Life to Slice of Life

--- category/slice-of-life.php
+++ category/slice-of-life.php
@@ -1,8 +1,8 @@
 <?php
 
 $CAT->title = array(
-	'EN' => "Personal Life",
-	'DE' => "Privatleben"
+	'EN' => "Slice of Life",
+	'DE' => "Slice of Life"
 );
 $CAT->description = array(
 	'EN' => "What happened to me, what I noticed and what I want to talk about",
@@ -13,7 +13,8 @@ $CAT->color = '#55f';
 $CAT->background = "url('./category/img/bg-programming.svg') no-repeat center, repeating-linear-gradient(-45deg, #55f, #55f 30px, #33f 30px, #33f 60px); background-size: contain;";
 
 $CAT->known = array(
-	"2016-05-16-abistreiche"
+	"2016-05-16-abistreiche",
+	"2016-05-30-elementary-odyssey"
 );
 
 return TRUE;
